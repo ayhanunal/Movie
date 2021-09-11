3038bc1c90387f11863fb21d284c8f181cecbfb0
ayhanunal <unalayhan@yahoo.com>
2021-09-11 22:17:45 +0300

    cast adapter view binding

--- app/src/main/java/com/ayhanunal/movies/adapter/CastAdapter.kt
+++ app/src/main/java/com/ayhanunal/movies/adapter/CastAdapter.kt
@@ -3,22 +3,25 @@ package com.ayhanunal.movies.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ayhanunal.movies.R
+import com.ayhanunal.movies.adapter.listener.CastClickListener
+import com.ayhanunal.movies.databinding.RowCastBinding
+import com.ayhanunal.movies.databinding.RowFeedBinding
 import com.ayhanunal.movies.model.Cast
 import com.ayhanunal.movies.util.downloadImage
 import com.ayhanunal.movies.util.placeholderProgressBar
 import com.ayhanunal.movies.view.MovieDetailsFragmentDirections
 import kotlinx.android.synthetic.main.row_cast.view.*
 
-class CastAdapter(private val list: List<Cast>) : RecyclerView.Adapter<CastAdapter.ViewHolder>() {
-
-    private val IMAGE_PATH = "https://image.tmdb.org/t/p/w185"
+class CastAdapter(private val list: List<Cast>) : RecyclerView.Adapter<CastAdapter.ViewHolder>(), CastClickListener {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.row_cast, parent, false)
+        val view = DataBindingUtil.inflate<RowCastBinding>(inflater, R.layout.row_cast, parent, false)
         return ViewHolder(view)
     }
 
@@ -27,14 +30,15 @@ class CastAdapter(private val list: List<Cast>) : RecyclerView.Adapter<CastAdapt
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.row_cast_actor_image.downloadImage(IMAGE_PATH + list[position].profile_path, placeholderProgressBar(holder.itemView.context))
-        holder.itemView.row_cast_actor_name.text = list[position].name
-
-        holder.itemView.setOnClickListener {
-            val action = MovieDetailsFragmentDirections.actionMovieDetailsFragmentToPersonDetailsFragment(list[position].id)
-            Navigation.findNavController(it).navigate(action)
-        }
+        holder.view.listener = this
+        holder.view.cast = list[position]
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+    override fun onCastClicked(v: View) {
+        val actorID = v.findViewById<TextView>(R.id.row_cost_actor_id).text.toString().toInt()
+        val action = MovieDetailsFragmentDirections.actionMovieDetailsFragmentToPersonDetailsFragment(actorID)
+        Navigation.findNavController(v).navigate(action)
+    }
+
+    class ViewHolder(var view: RowCastBinding): RecyclerView.ViewHolder(view.root)
 }
---
+++ app/src/main/java/com/ayhanunal/movies/adapter/listener/CastClickListener.kt
@@ -0,0 +1,9 @@
+package com.ayhanunal.movies.adapter.listener
+
+import android.view.View
+
+interface CastClickListener {
+
+    fun onCastClicked(v: View)
+
+}
