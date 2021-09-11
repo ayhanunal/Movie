fd600c03ee6a8a5f2bf3b107659227b28e1136ab
ayhanunal <unalayhan@yahoo.com>
2021-09-11 21:14:20 +0300

    feed row data binding

--- app/src/main/java/com/ayhanunal/movies/adapter/FeedAdapter.kt
+++ app/src/main/java/com/ayhanunal/movies/adapter/FeedAdapter.kt
@@ -3,22 +3,25 @@ package com.ayhanunal.movies.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ayhanunal.movies.R
+import com.ayhanunal.movies.databinding.RowFeedBinding
 import com.ayhanunal.movies.model.Result
 import com.ayhanunal.movies.util.downloadImage
 import com.ayhanunal.movies.util.placeholderProgressBar
 import com.ayhanunal.movies.view.FeedFragmentDirections
 import kotlinx.android.synthetic.main.row_feed.view.*
 
-class FeedAdapter(private val list: ArrayList<Result>) : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
+class FeedAdapter(private val list: ArrayList<Result>) : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>(), MovieClickListener {
 
     private val IMAGE_PATH = "https://image.tmdb.org/t/p/w185"
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.row_feed, parent, false)
+        val view = DataBindingUtil.inflate<RowFeedBinding>(inflater, R.layout.row_feed, parent, false)
         return FeedViewHolder(view)
     }
 
@@ -27,17 +30,12 @@ class FeedAdapter(private val list: ArrayList<Result>) : RecyclerView.Adapter<Fe
     }
 
     override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
-        holder.itemView.row_feed_movie_name.text = list[position].title
-        holder.itemView.row_feed_movie_language.text = "Language: ${list[position].original_language}"
-        holder.itemView.row_feed_movie_date.text = "Release Date: ${list[position].release_date}"
-        holder.itemView.row_feed_movie_popularity.text = "Popularity: ${list[position].popularity}"
-        holder.itemView.row_feed_movie_adult.text = "Adult: " + if (list[position].adult) "Yes" else "No"
-        holder.itemView.row_feed_movie_image.downloadImage(IMAGE_PATH + list[position].poster_path, placeholderProgressBar(holder.itemView.context))
 
-        holder.itemView.setOnClickListener {
-            val action = FeedFragmentDirections.actionFeedFragmentToMovieDetailsFragment(list[position].id)
-            Navigation.findNavController(it).navigate(action)
-        }
+        holder.view.listener = this
+        holder.view.movie = list[position]
+        holder.itemView.row_feed_movie_image.downloadImage(IMAGE_PATH + list[position].poster_path, placeholderProgressBar(holder.itemView.context))
+        holder.itemView.row_feed_movie_adult.text = "Adult: " + if (list[position].adult) "Yes" else "No"
+
     }
 
     fun updateList(updatedList: List<Result>) {
@@ -51,6 +49,11 @@ class FeedAdapter(private val list: ArrayList<Result>) : RecyclerView.Adapter<Fe
         notifyDataSetChanged()
     }
 
+    override fun onMovieClicked(v: View) {
+        val movieID = v.findViewById<TextView>(R.id.row_feed_movie_id).text.toString().toInt()
+        val action = FeedFragmentDirections.actionFeedFragmentToMovieDetailsFragment(movieID)
+        Navigation.findNavController(v).navigate(action)
+    }
 
-    class FeedViewHolder(view: View): RecyclerView.ViewHolder(view) { }
+    class FeedViewHolder(var view: RowFeedBinding): RecyclerView.ViewHolder(view.root) { }
 }
---
+++ app/src/main/java/com/ayhanunal/movies/adapter/MovieClickListener.kt
@@ -0,0 +1,9 @@
+package com.ayhanunal.movies.adapter
+
+import android.view.View
+
+interface MovieClickListener {
+
+    fun onMovieClicked(v: View)
+
+}
