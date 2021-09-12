6e3ca455cdb1e44de874156a1987e5de5fc4c46d
ayhanunal <unalayhan@yahoo.com>
2021-09-12 13:16:33 +0300

    movie api changes

--- app/src/main/java/com/ayhanunal/movies/service/MovieAPI.kt
+++ app/src/main/java/com/ayhanunal/movies/service/MovieAPI.kt
@@ -11,24 +11,24 @@ interface MovieAPI {
     //@GET("movie/popular?page={page}&api_key=01ec5c16d3a7d5c6af56d8e7d9e3af79")
 
     @GET("movie/popular")
-    fun getMovies(@Query("page") page: Int, @Query("api_key") api_key: String): Single<Movie>
+    fun getMovies(@Query("page") page: Int, @Query("api_key") apiKey: String): Single<Movie>
 
     @GET("movie/{id}/credits")
-    fun getActors(@Path("id")movieId: Int, @Query("api_key") api_key: String): Single<Actors>
+    fun getActors(@Path("id")movieId: Int, @Query("api_key") apiKey: String): Single<Actors>
 
     @GET("movie/{id}")
-    fun getMovieDetail(@Path("id")movieId: Int, @Query("api_key") api_key: String): Single<MovieDetail>
+    fun getMovieDetail(@Path("id")movieId: Int, @Query("api_key") apiKey: String): Single<MovieDetail>
 
     @GET("movie/{id}/videos")
-    fun getMovieVideos(@Path("id")movieId: Int, @Query("api_key") api_key: String): Single<MovieVideo>
+    fun getMovieVideos(@Path("id")movieId: Int, @Query("api_key") apiKey: String): Single<MovieVideo>
 
     @GET("search/movie")
-    fun searchMovies(@Query("page") page: Int, @Query("query") query: String, @Query("api_key") api_key: String): Single<Search>
+    fun searchMovies(@Query("page") page: Int, @Query("query") query: String, @Query("api_key") apiKey: String): Single<Search>
 
     @GET("person/{id}")
-    fun getActorDetail(@Path("id") actorId: Int, @Query("api_key") api_key: String): Single<ActorDetail>
+    fun getActorDetail(@Path("id") actorId: Int, @Query("api_key") apiKey: String): Single<ActorDetail>
 
     @GET("person/{id}/credits")
-    fun getPersonCredits(@Path("id") actorId: Int, @Query("api_key") api_key: String): Single<Credits>
+    fun getPersonCredits(@Path("id") actorId: Int, @Query("api_key") apiKey: String): Single<Credits>
 
 }
