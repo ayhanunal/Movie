b3e4eca2ef3fd4ea7c6dce0fe0f934bbaa40554d
ayhanunal <unalayhan@yahoo.com>
2021-09-12 13:26:02 +0300

    Update FeedViewModel.kt

--- app/src/main/java/com/ayhanunal/movies/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/ayhanunal/movies/viewmodel/FeedViewModel.kt
@@ -2,7 +2,6 @@ package com.ayhanunal.movies.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.ayhanunal.movies.model.Movie
 import com.ayhanunal.movies.model.Result
 import com.ayhanunal.movies.model.Search
@@ -23,58 +22,56 @@ class FeedViewModel(application: Application) : BaseViewModel(application) {
     var totalPage = MutableLiveData<Int>()
 
     fun refreshData(page: Int, query: String? = null) {
-        getDataFromAPI(page, query)
+        movieLoading.value = true
+        if (query != null) searchMoviesFromAPI(page, query) else getMoviesFromAPI(page)
     }
 
-    private fun getDataFromAPI(page: Int, query: String?) {
-        movieLoading.value = true
+    private fun searchMoviesFromAPI(page: Int, query: String){
+        disposable.add(
+            movieApiService.searchMovies(page, query)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<Search>() {
+                    override fun onSuccess(t: Search) {
+                        totalPage.value = t.total_pages
+                        movies.value = t.results
+                        movieError.value = false
+                        movieLoading.value = false
 
-        if (query == null) {
-            disposable.add(
-                movieApiService.getMovies(page)
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeWith(object : DisposableSingleObserver<Movie>() {
-                        override fun onSuccess(t: Movie) {
-                            totalPage.value = t.total_pages
-                            movies.value = t.results
-                            movieError.value = false
-                            movieLoading.value = false
+                        if (t.results.isEmpty()) { movieError.value = true }
+                    }
 
-                            if (t.results.isEmpty()) { movieError.value = true }
-                        }
+                    override fun onError(e: Throwable) {
+                        movieLoading.value = false
+                        movieError.value = true
+                        e.printStackTrace()
+                    }
+                })
+        )
+    }
 
-                        override fun onError(e: Throwable) {
-                            movieLoading.value = false
-                            movieError.value = true
-                            e.printStackTrace()
-                        }
-                    })
-            )
-        } else {
-            disposable.add(
-                movieApiService.searchMovies(page, query)
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeWith(object : DisposableSingleObserver<Search>() {
-                        override fun onSuccess(t: Search) {
-                            totalPage.value = t.total_pages
-                            movies.value = t.results
-                            movieError.value = false
-                            movieLoading.value = false
+    private fun getMoviesFromAPI(page: Int) {
+        disposable.add(
+            movieApiService.getMovies(page)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<Movie>() {
+                    override fun onSuccess(t: Movie) {
+                        totalPage.value = t.total_pages
+                        movies.value = t.results
+                        movieError.value = false
+                        movieLoading.value = false
 
-                            if (t.results.isEmpty()) { movieError.value = true }
-                        }
+                        if (t.results.isEmpty()) { movieError.value = true }
+                    }
 
-                        override fun onError(e: Throwable) {
-                            movieLoading.value = false
-                            movieError.value = true
-                            e.printStackTrace()
-                        }
-                    })
-            )
-        }
-
+                    override fun onError(e: Throwable) {
+                        movieLoading.value = false
+                        movieError.value = true
+                        e.printStackTrace()
+                    }
+                })
+        )
     }
 
     override fun onCleared() {
