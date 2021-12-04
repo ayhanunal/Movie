ec94c280598326fe67753844c159a9d84ef20ff5
ayhanunal <unalayhan@yahoo.com>
2021-12-04 15:47:20 +0300

    changed api key

--- app/src/main/java/com/ayhanunal/movies/util/Constants.kt
+++ app/src/main/java/com/ayhanunal/movies/util/Constants.kt
@@ -2,7 +2,7 @@ package com.ayhanunal.movies.util
 
 object Constants {
 
-    const val API_KEY = "01ec5c16d3a7d5c6af56d8e7d9e3af79"
+    const val API_KEY = "8ebda190411b279464efdc79f1ee09bc"
     const val BASE_URL = "https://api.themoviedb.org/3/"
 
 }
