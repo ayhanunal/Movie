ef3533d49f3ebbe61e18e537587107bbd95f5bb1
ayhanunal <unalayhan@yahoo.com>
2021-12-04 17:41:42 +0300

    API response language has been changed according to locale

--- app/src/main/java/com/ayhanunal/movies/service/MovieAPIService.kt
+++ app/src/main/java/com/ayhanunal/movies/service/MovieAPIService.kt
@@ -3,12 +3,20 @@ package com.ayhanunal.movies.service
 import com.ayhanunal.movies.model.*
 import com.ayhanunal.movies.util.Constants.API_KEY
 import com.ayhanunal.movies.util.Constants.BASE_URL
+import com.ayhanunal.movies.util.Languages
+import com.ayhanunal.movies.view.MainActivity
 import io.reactivex.Single
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MovieAPIService {
+
+    private var apiResponseLang: String = when(MainActivity.APP_LOCALE_LANGUAGE){
+        "tr" -> Languages.TURKISH.lang
+        "gr" -> Languages.GERMAN.lang
+        else -> Languages.ENGLISH.lang
+    }
 
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
@@ -18,31 +26,31 @@ class MovieAPIService {
         .create(MovieAPI::class.java)
 
     fun getMovies(page: Int): Single<Movie> {
-        return api.getMovies(page, API_KEY)
+        return api.getMovies(page, API_KEY, apiResponseLang)
     }
 
     fun searchMovies(page: Int, query: String): Single<Search> {
-        return api.searchMovies(page, query, API_KEY)
+        return api.searchMovies(page, query, API_KEY, apiResponseLang)
     }
 
     fun getActors(id: Int): Single<Actors> {
-        return api.getActors(id, API_KEY)
+        return api.getActors(id, API_KEY, apiResponseLang)
     }
 
     fun getMovieDetail(id: Int): Single<MovieDetail> {
-        return api.getMovieDetail(id, API_KEY)
+        return api.getMovieDetail(id, API_KEY, apiResponseLang)
     }
 
     fun getActorDetail(id: Int): Single<ActorDetail> {
-        return api.getActorDetail(id, API_KEY)
+        return api.getActorDetail(id, API_KEY, apiResponseLang)
     }
 
     fun getPersonCredits(id: Int): Single<Credits> {
-        return api.getPersonCredits(id, API_KEY)
+        return api.getPersonCredits(id, API_KEY, apiResponseLang)
     }
 
     fun getMovieVideos(id: Int): Single<MovieVideo> {
-        return api.getMovieVideos(id, API_KEY)
+        return api.getMovieVideos(id, API_KEY, apiResponseLang)
     }
 
 }
--- app/src/main/java/com/ayhanunal/movies/view/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/movies/view/MainActivity.kt
@@ -6,10 +6,19 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.ayhanunal.movies.R
+import android.app.Activity
+import android.content.res.Configuration
+import android.content.res.Resources
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navigationController: NavController
+
+    companion object{
+        var APP_LOCALE_LANGUAGE: String = Locale.getDefault().language
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,4 +32,5 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navigationController, null)
     }
+    
 }
